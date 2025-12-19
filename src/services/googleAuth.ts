7b750480ec548c20957016519b55baf03ad93197
thiago <thiago.w3c@gmail.com>
2025-12-19 18:46:53 -0300

    client id

--- src/services/googleAuth.ts
+++ src/services/googleAuth.ts
@@ -13,7 +13,7 @@ export function useGoogleAuth(onLogin?: () => void) {
   const [request, response, promptAsync] = Google.useAuthRequest({
     clientId: "1026415452462-bnqbtkpks7pts26n6l4eg22en1pradau.apps.googleusercontent.com",
     webClientId: "1026415452462-bnqbtkpks7pts26n6l4eg22en1pradau.apps.googleusercontent.com",
-    androidClientId: "1026415452462-v95o9cb87b6va3kopvq5sagpsvratmtd.apps.googleusercontent.com",
+    androidClientId: "1026415452462-5fq481navlqg8pon3sobamd9l6nmgjb.apps.googleusercontent.com",
     iosClientId: "1026415452462-3jti3vafhr81mjkrmftdv11edugdgm42.apps.googleusercontent.com",
     scopes: ["openid", "profile", "email"],
     responseType: "id_token", // <-- ESSENCIAL!
